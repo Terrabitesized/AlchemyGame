3b9e8b18c1398fdb0bf42fbefe9032525dd12c63
Terrabitesized <finnsmart7@gmail.com>
2025-08-06 15:04:33 -0400

    Extra Enemy Attack

--- DanceAtSunset/Assets/Scripts/Combat Scripts/EnemyAI/TestAI1.cs
+++ DanceAtSunset/Assets/Scripts/Combat Scripts/EnemyAI/TestAI1.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -6,8 +7,9 @@ public class TestAI1 : MonoBehaviour
 {
     public Vector3[] PuddlePositions;
 
-    [SerializeField] private float atkWaitTime = 0.5f;
+    private Vector3 targetPos;
 
+   
     //bool onCooldown = false;
     bool alive = true;
     [SerializeField] private float atkCooldown = 5f;
@@ -16,10 +18,26 @@ public class TestAI1 : MonoBehaviour
     [SerializeField] private GameObject hurtPuddle;
     [SerializeField] private GameObject warnPuddle;
 
+    [SerializeField] private float atkWaitTime1 = 0.5f;
+
     [SerializeField] private int hurtPuddleDamage = 5;
     [SerializeField] private float hurtPuddleDamageCooldown = .5f;
 
+    [Header("Attack 2 Variables")]
+    [SerializeField] private GameObject hurtArea;
+    [SerializeField] private GameObject warnArea;
 
+    private Vector3 savedPos;
+
+    [SerializeField] private float atkWaitTime2 = 0.5f;
+
+
+    [SerializeField] private int hurtAreaDamage = 5;
+
+    private void Awake()
+    {
+        
+    }
 
     void Start()
     {
@@ -44,15 +62,17 @@ public class TestAI1 : MonoBehaviour
             // Multiple "corrosive puddles" are set around the arena.
             if (atkChoice == 1)
             {
-                hurtPuddles();
+                slimeAttack1();
             } else if (atkChoice == 2)
             {
-
+                slimeAttack2();
             }
         }
     }
 
-    private void hurtPuddles()
+    // ATTACK 1 START
+
+    private void slimeAttack1()
     {
 
         PuddlePositions = new Vector3[10];
@@ -76,12 +96,12 @@ public class TestAI1 : MonoBehaviour
 
 
         }
-        StartCoroutine("placeAttack");
+        StartCoroutine("placeAttack1");
     }
 
-    private IEnumerator placeAttack()
+    private IEnumerator placeAttack1()
     {
-        yield return new WaitForSeconds(atkWaitTime);
+        yield return new WaitForSeconds(atkWaitTime1);
 
        
 
@@ -96,7 +116,38 @@ public class TestAI1 : MonoBehaviour
        
         temp.transform.position = new Vector3(PuddlePositions[i].x, 0, PuddlePositions[i].z);
         }
-
-        
     }
+
+    // ATTACK 1 END
+
+    // ATTACK 2 START
+
+    private void slimeAttack2()
+    {
+        GameObject temp = Instantiate(warnArea);
+
+        targetPos = GameObject.FindWithTag("Player").transform.position;
+
+        savedPos = new Vector3(targetPos.x, 0, targetPos.z);
+
+        temp.transform.position = savedPos;
+
+        StartCoroutine("placeAttack2");
+    }
+
+    private IEnumerator placeAttack2()
+    {
+        yield return new WaitForSeconds(atkWaitTime2);
+
+        GameObject temp = Instantiate(hurtArea);
+
+
+        temp.GetComponent<AttackAttributes>().damage = hurtAreaDamage;
+        temp.GetComponent<AttackAttributes>().damageCooldown = 5;
+
+        temp.transform.position = savedPos;
+    }
+
+
+
 }
