556f999f5d2e9de95ccf199f77a4ab9489aca82a
NathanAnthony <anthonnp@miamioh.edu>
2026-08-31 16:41:53 -0400

    Update EnemyAttackPattern.cs

--- DanceAtSunset/Assets/Scripts/Combat Scripts/Interfaces/EnemyAttackPattern.cs
+++ DanceAtSunset/Assets/Scripts/Combat Scripts/Interfaces/EnemyAttackPattern.cs
@@ -11,7 +11,7 @@ public abstract class EnemyAttackPattern
     public string AttackName;
 
     public float AttackCastTime;
-    public float AttackingCooldown;
+    public float AdditionalAttackCooldown;
 
     public GameObject WarningPrefab;
     public GameObject AttackPrefab;
